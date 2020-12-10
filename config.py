59d89e289c63f121f055d4701369c4dae96cab8f
Artem Kovyryagin <dastet@rambler.ru>
2020-12-10 19:08:03 +0300

    add CRUD and connect to db

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app\data.db')
+    print(SQLALCHEMY_DATABASE_URI)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

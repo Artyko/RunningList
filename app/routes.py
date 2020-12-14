b1f7a43eff4f72f4e56e21dc6675ba3c735266b4
Artem Kovyryagin <dastet@rambler.ru>
2020-12-14 23:09:11 +0300

    add week checkboxes

--- app/routes.py
+++ app/routes.py
@@ -27,6 +27,16 @@ def return_all_tasks():
     return serialized_results
 
 
+def update_day(task_id, payload):
+    task_to_update = db.session.query(Task).filter_by(id=task_id)
+    task_to_update.update(payload)
+    db.session.commit()
+
+
+def change_true_to_True(data):
+    return {key: (True if value == 'true' else False) for key, value in data.items()}
+
+
 @task_routes.route('/books', methods=['GET', 'POST'])
 def all_books():
     response_object = {'status': 'success'}
@@ -50,4 +60,14 @@ def single_book(book_id):
     if request.method == 'DELETE':
         remove_task(book_id)
         response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+
+@task_routes.route('/books/day/<book_id>', methods=['POST'])
+def single_book_day(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        normalized_data = change_true_to_True(post_data)
+        update_day(book_id, normalized_data)
     return jsonify(response_object)

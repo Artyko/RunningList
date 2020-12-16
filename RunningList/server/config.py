d98def86c642b2a8ee3d2509b0485b1806522773
Artem Kovyryagin <dastet@rambler.ru>
2020-12-16 18:00:46 +0300

    update flask config

--- RunningList/server/config.py
+++ RunningList/server/config.py
@@ -8,5 +8,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app\data.db')
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False

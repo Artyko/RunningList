fcfecf537d6f192d6ec6797e5c819ede7ce29b1f
Artem Kovyryagin <dastet@rambler.ru>
2020-12-16 18:42:24 +0300

    fix DeprecationWarning

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app\data.db')
+                              'sqlite:///' + os.path.join(basedir, 'app/data.db')
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False

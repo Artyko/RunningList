34958132b76dd8ad48a85915061959e110261f4d
Artem Kovyryagin <dastet@rambler.ru>
2020-12-20 20:03:54 +0300

    add redirect from main page

--- RunningList/server/config.py
+++ RunningList/server/config.py
@@ -1,12 +1,11 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app/data.db')
-    print(SQLALCHEMY_DATABASE_URI)
+                              'sqlite:///' + os.path.join(BASEDIR, 'app/data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
